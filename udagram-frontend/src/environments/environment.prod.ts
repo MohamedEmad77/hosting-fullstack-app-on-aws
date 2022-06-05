598ac6fba81be3102fc9da088e52c85f2e44e8e6
mohamed  emad <mohamademad97@yahoo.com>
2022-06-05 03:07:05 +0200

    Update environment.prod.ts

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  apiHost: "mohamedemad-api-dev.eba-wxgub8xc.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
